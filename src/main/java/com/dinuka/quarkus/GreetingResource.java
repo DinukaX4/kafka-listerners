063d25fa45025432d9c828667832efaf714c0258
Thisal <dinukathisal.1995@gmail.com>
2026-01-31 21:40:17 +0400

    Added open-telemetry.

--- src/main/java/com/dinuka/quarkus/GreetingResource.java
+++ src/main/java/com/dinuka/quarkus/GreetingResource.java
@@ -1,16 +1,22 @@
 package com.dinuka.quarkus;
 
+import org.slf4j.Logger;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+
 @Path("/hello")
 public class GreetingResource {
+
+  Logger logger = org.slf4j.LoggerFactory.getLogger(GreetingResource.class);
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        logger.info("Received request for /hello endpoint");
         return "Hello from Quarkus REST";
     }
 }
